caea2300f51a58a8bb19904f7ccb35110bbe697c
IlyaPluh <85877006+IlyaPluh@users.noreply.github.com>
2023-06-08 21:56:32 +0300

    create js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
+import { promises as fs } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const folderPath = 'src/fs/files/';
+  const filePath = folderPath + 'fresh.txt';
+
+  try {
+    // Check if the file already exists
+    await fs.access(filePath);
+
+    // File already exists, throw an error
+    throw new Error('FS operation failed. The file already exists.');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      // File does not exist, create it with the content
+      await fs.writeFile(filePath, 'I am fresh and young');
+      console.log('Fresh file created successfully!');
+    } else {
+      // Other error occurred
+      throw error;
+    }
+  }
 };
 
-await create();
+// Usage
+(async () => {
+  try {
+    await create();
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
